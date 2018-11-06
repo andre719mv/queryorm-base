5753d5966c060beed57a4ff31a745893a2d309af
Andrii Meretskyi <andre719mv81@gmail.com>
2018-11-07 01:10:02 +0200

    better support for float

--- base/src/main/java/com/stayfit/queryorm/lib/DOBase.java
+++ base/src/main/java/com/stayfit/queryorm/lib/DOBase.java
@@ -48,11 +48,16 @@ public class DOBase {
 					f.setLong(this,	cursor.getLong(cursor.getColumnIndex(column)));
 				} else if (fieldType.equals("java.util.Date")) {
 					f.set(this,	new TypeConverter().readDate(cursor.getString(cursor.getColumnIndex(column))));
-				}else if (fieldType.equals("double") || fieldType.equals("float")) {
+				}else if (fieldType.equals("double")) {
 					f.set(this,	cursor.getDouble(cursor.getColumnIndex(column)));
-				}else if (fieldType.equals("java.lang.Double") || fieldType.equals("java.lang.Float")) {
+				}else if (fieldType.equals("java.lang.Double")) {
 					if (!cursor.isNull(cursor.getColumnIndex(column)))
 						f.set(this, cursor.getDouble(cursor.getColumnIndex(column)));
+				}else if (fieldType.equals("float")) {
+					f.set(this,	(float)cursor.getDouble(cursor.getColumnIndex(column)));
+				}else if (fieldType.equals("java.lang.Float")) {
+					if (!cursor.isNull(cursor.getColumnIndex(column)))
+						f.set(this, (float)cursor.getDouble(cursor.getColumnIndex(column)));
 				}else{
 					throw new RuntimeException("Unexpected field type " + fieldType + " mapped to " + tableName + "." + column);
 				}
